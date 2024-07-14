77ffc2c6a7ded4aed52fa401e06ac8068be1baaa
harrychong <harryhuayuanchong@gmail.com>
2024-07-14 16:48:39 +0800

    feat: Header (In progress)

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,8 +1,21 @@
+import Link from 'next/link'
+import Image from 'next/image'
 import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='w-full border-b'>
+        <div className='wrapper flex items-center justify-between'>
+            <Link href="/">
+                <Image 
+                    src="/assets/images/FundM3-Logo-01.jpg"
+                    alt='FundM3 Logo'
+                    width={128}
+                    height={38}
+                />
+            </Link>
+        </div>
+    </header>
   )
 }
 
