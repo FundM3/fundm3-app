80e4f53aed7adc75ce50ac150582f01da918b79b
harrychong <harryhuayuanchong@gmail.com>
2024-07-15 12:50:25 +0800

    fix: Projects & Creators styling issues

--- src/app/(root)/creators/page.tsx
+++ src/app/(root)/creators/page.tsx
@@ -4,7 +4,13 @@ import React from 'react'
 
 const Creators = () => {
   return (
-    <div>Creators Page</div>
+    <>
+        <section className="bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+            <div className="wrapper flex items-center justify-center sm:justify-between">
+                <h3 className='h3-bold text-center sm:text-left'>Creators</h3>
+            </div>
+        </section>
+    </>
   )
 }
 
