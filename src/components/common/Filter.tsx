ba64bd4ec9a19f07303391ac53dc5cc68a685d00
harrychong <harryhuayuanchong@gmail.com>
2024-07-16 11:32:43 +0800

    feat: Homepage, Filter, Search

---
+++ src/components/common/Filter.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+import React, { useEffect, useState } from 'react'
+import { useRouter, useSearchParams } from "next/navigation";
+
+const Filter = () => {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+
+    return (
+        <Select>
+          <SelectTrigger className="select-field">
+            <SelectValue placeholder="Category" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="all" className="select-item p-regular-14">All</SelectItem>
+            <SelectItem value="projects" className="select-item p-regular-14">Projects</SelectItem>
+            <SelectItem value="creators" className="select-item p-regular-14">Creators</SelectItem>
+          </SelectContent>
+        </Select>
+      )
+}
+
+export default Filter
