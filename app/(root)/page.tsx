640321f4f371ef3ee1bc03360548e41ec11484c4
harrychong <harryhuayuanchong@gmail.com>
2024-07-28 14:51:35 +0800

    update: landing page background

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
 
   return (
     <>
-      <section className="bg-yellow bg-dotted-pattern bg-contain py-5 md:py-10">
+      <section className="bg-custom-pattern bg-cover bg-center bg-yellow bg-dotted-pattern bg-contain py-5 md:py-10">
         <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
             <h1 className="h1-bold">Empower Dreams, Fuel Change</h1>
@@ -32,11 +32,6 @@ export default function Home() {
                 Explore More
               </Link>
             </Button>
-            {/* <Button size="lg" asChild className="button w-full sm:w-fit">
-              <Link href="/creators">
-                Explore Creators
-              </Link>
-            </Button> */}
           </div>
 
           <Image 
@@ -68,3 +63,12 @@ export default function Home() {
     </>
   );
 }
+
+
+{/* <Image
+  src="/assets/images/background.svg"
+  alt="background"
+  width={1000}
+  height={1000}
+  className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
+/>  */}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -73,6 +73,9 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      backgroundImage: {
+        'custom-pattern': "url('/assets/images/background.svg')"
+      }
     },
   },
   plugins: [require("tailwindcss-animate")],
