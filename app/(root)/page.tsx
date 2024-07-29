096fbd3882465f3814ea205f2c1f337095c0acd1
harrychong <harryhuayuanchong@gmail.com>
2024-07-29 09:15:41 +0800

    fix: remove redundant code

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -46,8 +46,6 @@ export default function Home() {
       </section> 
 
       <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
-        {/* <h2 className="h2-bold">Explore <br /> Projects or Creators </h2> */}
-
         <div className="flex w-full flex-col gap-5 md:flex-row">
           <Search />
           <Filter />
