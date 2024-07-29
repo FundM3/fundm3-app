d923cbb028126e8a7e17542705631ce315106df1
harrychong <harryhuayuanchong@gmail.com>
2024-07-29 13:51:19 +0800

    fix: coding style

--- app/(root)/create/page.tsx
+++ app/(root)/create/page.tsx
@@ -6,15 +6,15 @@ import React from 'react'
 const CreateProject = () => {
   return (
     <>
-        <section className="bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
-            <div className="wrapper flex items-center justify-center sm:justify-between">
-                <h3 className='h3-bold text-center sm:text-left'>Create Projects</h3>
-            </div>
-        </section>
-
-        <div className="wrapper my-8">
-            <CreateProjectForm />
+      <section className="bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <div className="wrapper flex items-center justify-center sm:justify-between">
+          <h3 className='h3-bold text-center sm:text-left'>Create Projects</h3>
         </div>
+      </section>
+
+      <div className="wrapper my-8">
+        <CreateProjectForm />
+      </div>
     </>
   )
 }
--- app/(root)/projects/page.tsx
+++ app/(root)/projects/page.tsx
@@ -8,25 +8,21 @@ import React from 'react'
 const Projects = () => {
   return (
     <>
-        <section className="bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
-            <div className="wrapper flex items-center justify-center sm:justify-between">
-              <h3 className='h3-bold text-center sm:text-left'>Projects</h3>
-            </div>
+      <section className="bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+          <div className="wrapper flex items-center justify-center sm:justify-between">
+            <h3 className='h3-bold text-center sm:text-left'>Projects</h3>
+          </div>
 
-            <div className="wrapper flex items-center justify-center sm:justify-between w-full flex-col gap-5 md:flex-row">
-              <Search />
-              <SearchButton />
-              
-            </div>
+          <div className="wrapper flex items-center justify-center sm:justify-between w-full flex-col gap-5 md:flex-row">
+            <Search />
+            <SearchButton />
             
-            <div className="wrapper flex items-center justify-center sm:justify-between">
-              <ProjectsCollection />
-            </div>
-        </section>
-
-        <section>
-
-        </section>
+          </div>
+          
+          <div className="wrapper flex items-center justify-center sm:justify-between">
+            <ProjectsCollection />
+          </div>
+      </section>
     </>
   )
 }
