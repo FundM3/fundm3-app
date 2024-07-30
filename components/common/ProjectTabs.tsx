00f737dd109b525618a9f1818affef6aa7660ebb
harrychong <harryhuayuanchong@gmail.com>
2024-07-30 10:28:06 +0800

    feat: ProjectTabs & SponsorList component

--- components/common/ProjectTabs.tsx
+++ components/common/ProjectTabs.tsx
@@ -17,11 +17,12 @@ import {
     TableRow,
 } from "@/components/ui/table"
 import Image from 'next/image'
+import SponsorList from './SponsorList'
 
 const ProjectTabs = () => {
   return (
     <>
-      <section className='bg-dotted-pattern bg-cover bg-center py-5 md:py-10 px-[70px]'>
+      <section className='bg-dotted-pattern bg-cover bg-center py-5 md:py-10 lg:px-[70px]'>
         <Tabs defaultValue="1">
             <div className="flex items-center">
                 <TabsList>
@@ -55,112 +56,13 @@ const ProjectTabs = () => {
             <TabsContent value="2">
                 <Card x-chunk="dashboard-05-chunk-3">
                     <CardHeader className="px-7">
-                        <CardTitle>Sponsor List</CardTitle>
-                        <CardDescription>
-                            Recent sponsor from your donators.
-                        </CardDescription>
+                      <CardTitle>Sponsor List</CardTitle>
+                      <CardDescription>
+                          Recent sponsor from your donators.
+                      </CardDescription>
                     </CardHeader>
                     <CardContent>
-                        <Table>
-                            <TableHeader>
-                            <TableRow>
-                                <TableHead>Sponsor By</TableHead>
-                                {/* <TableHead className="hidden sm:table-cell">
-                                Status
-                                </TableHead> */}
-                                <TableHead className="hidden md:table-cell">
-                                Date
-                                </TableHead>
-                                <TableHead className="text-right">Amount</TableHead>
-                            </TableRow>
-                            </TableHeader>
-                            <TableBody>
-                            <TableRow className="bg-accent">
-                                <TableCell>
-                                    <div className="font-medium">Liam Johnson</div>
-                                    <div className="hidden text-sm text-muted-foreground md:inline">
-                                        liam@example.com
-                                    </div>
-                                </TableCell>
-                                <TableCell className="hidden md:table-cell">
-                                2023-06-23
-                                </TableCell>
-                                <TableCell className="text-right">$250.00</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell>
-                                <div className="font-medium">Olivia Smith</div>
-                                <div className="hidden text-sm text-muted-foreground md:inline">
-                                    olivia@example.com
-                                </div>
-                                </TableCell>
-                                <TableCell className="hidden md:table-cell">
-                                2023-06-24
-                                </TableCell>
-                                <TableCell className="text-right">$150.00</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell>
-                                    <div className="font-medium">Noah Williams</div>
-                                    <div className="hidden text-sm text-muted-foreground md:inline">
-                                        noah@example.com
-                                    </div>
-                                </TableCell>
-                                <TableCell className="hidden md:table-cell">
-                                2023-06-25
-                                </TableCell>
-                                <TableCell className="text-right">$350.00</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell>
-                                <div className="font-medium">Emma Brown</div>
-                                <div className="hidden text-sm text-muted-foreground md:inline">
-                                    emma@example.com
-                                </div>
-                                </TableCell>
-                                <TableCell className="hidden md:table-cell">
-                                2023-06-26
-                                </TableCell>
-                                <TableCell className="text-right">$450.00</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell>
-                                <div className="font-medium">Liam Johnson</div>
-                                <div className="hidden text-sm text-muted-foreground md:inline">
-                                    liam@example.com
-                                </div>
-                                </TableCell>
-                                <TableCell className="hidden md:table-cell">
-                                2023-06-23
-                                </TableCell>
-                                <TableCell className="text-right">$250.00</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell>
-                                <div className="font-medium">Olivia Smith</div>
-                                <div className="hidden text-sm text-muted-foreground md:inline">
-                                    olivia@example.com
-                                </div>
-                                </TableCell>
-                                <TableCell className="hidden md:table-cell">
-                                2023-06-24
-                                </TableCell>
-                                <TableCell className="text-right">$150.00</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell>
-                                <div className="font-medium">Emma Brown</div>
-                                <div className="hidden text-sm text-muted-foreground md:inline">
-                                    emma@example.com
-                                </div>
-                                </TableCell>
-                                <TableCell className="hidden md:table-cell">
-                                2023-06-26
-                                </TableCell>
-                                <TableCell className="text-right">$450.00</TableCell>
-                            </TableRow>
-                            </TableBody>
-                        </Table>
+                      <SponsorList />
                     </CardContent>
                 </Card>
             </TabsContent>
