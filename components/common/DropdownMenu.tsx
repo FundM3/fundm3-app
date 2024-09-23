fafe91df153a5552bc46cbe589ef12478a893441
DecXlll <like861213@gmail.com>
2024-09-23 21:31:48 +0800

    feat:dropdown menu  add disconnect button.

--- components/common/DropdownMenu.tsx
+++ components/common/DropdownMenu.tsx
@@ -1,44 +1,48 @@
-import React from 'react'
+import React from "react";
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-    DropdownMenuShortcut,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+  DropdownMenuShortcut,
 } from "@/components/ui/dropdown-menu";
-import Link from 'next/link';
+import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { useAccount } from "wagmi";
+import { useAccount, useDisconnect } from "wagmi";
 
 const Dropdown = () => {
-    const { address } = useAccount()
+  const { address } = useAccount();
+  const { disconnect } = useDisconnect();
 
   return (
-    <div className='ml-2 pt-[1px]'>
-        <DropdownMenu>
-            <DropdownMenuTrigger>
-            <div className='pt-0.5'>
-                <Avatar>
-                <AvatarImage src="/assets/icons/profile.svg" alt="@shadcn" />
-                <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-            </div>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-            {/* <DropdownMenuLabel>My Account</DropdownMenuLabel>
+    <div className="ml-2 pt-[1px]">
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <div className="pt-0.5">
+            <Avatar>
+              <AvatarImage src="/assets/icons/profile.svg" alt="@shadcn" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </div>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          {/* <DropdownMenuLabel>My Account</DropdownMenuLabel>
             <DropdownMenuSeparator /> */}
-            <Link href="/profile" >
-                <DropdownMenuItem>My Profile</DropdownMenuItem>
-            </Link>
-            <Link href={`/creators/${address}`}>
-                <DropdownMenuItem>My Donors</DropdownMenuItem>
-            </Link>
-            </DropdownMenuContent>
-        </DropdownMenu>
+          <Link href="/profile">
+            <DropdownMenuItem>My Profile</DropdownMenuItem>
+          </Link>
+          <Link href={`/creators/${address}`}>
+            <DropdownMenuItem>My Donors</DropdownMenuItem>
+          </Link>
+          <DropdownMenuItem>
+            <button onClick={() => disconnect()}>Disconnect</button>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
-  )
-}
+  );
+};
 
-export default Dropdown
+export default Dropdown;
